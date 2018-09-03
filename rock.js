bba21feb6935c45f0a66670afd0c6d04b8a480ab
Casey Calkins <c.angelcalkins@outlook.com>
2018-09-03 13:20:29 -0500

    Finished/Working Rock, Paper, Scissors game through the Console.

--- rock.js
+++ rock.js
@@ -1,4 +1,6 @@
 let gameChoice = ['rock', 'paper', 'scissors'];
+let playerScore = 0;
+let computerScore = 0;
 
 function computerPlay(playerSelection, computerSelection) {
 	let randomNumber = Math.floor(Math.random()*gameChoice.length);
@@ -11,12 +13,27 @@ function playGame(playerSelection, computerSelection) {
 	if (playerSelection === "rock" && computerSelection === "scissors" ||
 		playerSelection === "scissors" && computerSelection === "paper"||
 		playerSelection === "paper" && computerSelection === "rock") {
+			playerScore += 1;
 			return playerSelection + " beats " + computerSelection + ".";
 	} else if (playerSelection === "scissors" && computerSelection === "rock" ||
 			   playerSelection === "rock" && computerSelection === "paper" ||
 			   playerSelection === "paper" && computerSelection === "scissors") {
+				   computerScore += 1;
 				   return playerSelection + " loses to " + computerSelection + ".";
 			   } else {
 				   return "Draw";
 			   }
 }
+
+function game() {
+	for (let i = 0; i < 5; i++) {
+		console.log(playGame());
+	}
+	if (playerScore > computerScore) {
+		console.log("Player wins with " + playerScore + " points.");
+	} else if (computerScore > playerScore) {
+		console.log("Computer wins with " + computerScore + " points.");
+	}
+}
+	
+	
